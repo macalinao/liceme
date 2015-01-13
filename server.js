47d4badec6db502e642d6167c39cbb72309e029f
Ian Macalinao <me@ian.pw>
2015-01-13 09:53:58 -0600

    Add server directory

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-var express = require('express');
-
-var app = express();
-
-app.use(express.static('dist/'));
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log('Listening on port', port);
-});
+require('./server/app');
